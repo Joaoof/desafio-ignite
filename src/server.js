01c5fd95e0569c20b58708272500fd8f97566246
Joaoof <joaodeus400@gmail.com>
2023-02-25 13:11:44 -0300

    middlewares de JSON

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from  'node:http'
+import { json } from './middlewares/json.js'
 
 /* 
 1. Criação de uma task
@@ -14,21 +15,10 @@ const server = http.createServer(async (req, res) => {
 
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if(method == 'GET' && url == '/tasks') {
     return res
-    .setHeader('Content-type', 'application/json')
     .end(JSON.stringify(tasks))
   }
 
