7fecadbc3f1868e2c5d7f5aec4f84db6926761ee
Joaoof <joaodeus400@gmail.com>
2023-02-25 16:53:19 -0300

    ainda select

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -1,7 +1,11 @@
 export class Database {
   database = {}
 
-  select(table) // tabela que quero selecionar, e ele vai retornar todos os dados contido dentro desta tabela.
+  select(table) {
+    const data = this.database[table] ?? []
+
+    return data
+  } // tabela que quero selecionar, e ele vai retornar todos os dados contido dentro desta tabela.
  
   insert(table, data) // Esse insert vai receber a tabela do banco que eu qeuro fazer inserção e, os dados também.
 }
