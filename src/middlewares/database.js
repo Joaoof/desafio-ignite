874b87a5e7349252acadc9fd1c5c2333d3b7fc3f
Joaoof <joaodeus400@gmail.com>
2023-02-25 16:49:03 -0300

    add method select

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -1,5 +1,7 @@
 export class Database {
   database = {}
 
+  select(table) // tabela que quero selecionar, e ele vai retornar todos os dados contido dentro desta tabela.
+ 
   insert(table, data) // Esse insert vai receber a tabela do banco que eu qeuro fazer inserção e, os dados também.
 }
